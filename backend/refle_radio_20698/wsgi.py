ec1fd43bceaa506367bc4b7864059ef5894f092a
Crowdbotics <team@crowdbotics.com>
2020-09-25 21:57:06 +0000

    Update models

--- backend/refle_radio_20698/wsgi.py
+++ backend/refle_radio_20698/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'refle_radio_20698.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "refle_radio_20698.settings")
 
 application = get_wsgi_application()
