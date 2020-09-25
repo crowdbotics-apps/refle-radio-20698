3dc0846ed92e57183fc88973dc49619643b7373e
Crowdbotics <team@crowdbotics.com>
2020-09-25 21:56:01 +0000

    Initial commit.

---
+++ ios/refle_radio_20698Tests/refle_radio_20698Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface refle_radio_20698 : XCTestCase
+
+@end
+
+@implementation refle_radio_20698
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
